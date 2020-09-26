5023ea1a4dfeb36dd6bf39bf26a2c44202b5984d
Samuel Cedarbaum <scedarbaum@gmail.com>
2020-09-25 21:25:19 -0700

    Fix last commit

--- org-gtasks.el
+++ org-gtasks.el
@@ -1,23 +1,31 @@
 ;;; org-gtasks.el -- Export/import all Google Tasks to org files.
 
 ;; Copyright (C) 2018-2019 Julien Masson
-
+;;
 ;; Author: Julien Masson <massonju.eseo@gmail.com>
-;; URL: https://github.com/JulienMasson/org-gtasks
-;;; Commentary:
 
-;; This program is free software; you can redistribute it and/or modify
+;; Package-Requires: ((emacs "25.1"))
+;; Keywords: org tasks google
+;; Homepage: https://github.com/JulienMasson/org-gtasks
+
+;; This file is not part of GNU Emacs.
+
+;; This file is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
-;; (at your option) any later version.
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
 
-;; This program is distributed in the hope that it will be useful,
+;; This file is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
 
 ;; You should have received a copy of the GNU General Public License
-;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;; along with this file.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Org mode integration with Google Tasks.
 
 ;;; Code:
 
